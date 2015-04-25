749f7f86936b64e8c46dd4cff9018644a69b781d
Schneider, Alexander <alexander.schneider@hhu.de>
2015-04-25 19:53:39 +0200

    first try at a model for teams

---
+++ teams/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Team(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    date_created = model.DateTime(auto_now_add=True)
+    institution = model.CharField(max_length=124) # university where team is from
+    tag = models.CharField(max_length=16)
+    description = models.TextField(max_length=1024)
+    logo = models.ImageField()
+
+class Player(models.Model):
+    user = models.OneToOneField(User)
+    team = ForeignKey('Team')
