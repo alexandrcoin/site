3c9fa165e8939a291e3842d350e5e7b258c9f514
Schneider, Alexander <alexander.schneider@hhu.de>
2015-04-25 23:15:21 +0200

    add dummy detail view for teams

--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Team
+from .models import User
 
 
 def index(request):
@@ -8,3 +10,17 @@ def index(request):
     for i, teamobj in enumerate(Team.objects.all()):
         response += "Team %i: %s <br>" % (i, teamobj)
     return HttpResponse(response)
+
+def detail(request, teamid):
+    try:
+        name = Team.objects.get(id=teamid).name
+        response = 'Team: %s <br>' % name
+        response += 'Members: <br>'
+        users = User.objects.filter(team__name=name)
+        for username in users:
+            response += '  * %s <br>' % username
+    except ObjectDoesNotExist:
+        response = 'fuck you'
+
+    return HttpResponse(response)
+
